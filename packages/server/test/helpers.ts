984420d181275df2dbf7a1554c6ffef4bda7aa70
Kyle Carberry <kyle@kwc.io>
2019-01-11 13:58:31 -0600

    Convert indentation to tabs

--- packages/server/test/helpers.ts
+++ packages/server/test/helpers.ts
@@ -3,26 +3,26 @@ import { Client } from "../src/browser/client";
 import { Server } from "../src/node/server";
 
 export const createClient = (): Client => {
-    const s2c = new Emitter<Uint8Array | Buffer>();
-    const c2s = new Emitter<Uint8Array | Buffer>();
+	const s2c = new Emitter<Uint8Array | Buffer>();
+	const c2s = new Emitter<Uint8Array | Buffer>();
 
-    new Server({
-        close: () => undefined,
-        onClose: () => undefined,
-        onMessage: (cb) => {
-            c2s.event((d) => cb(d));
-        },
-        send: (data) => setTimeout(() => s2c.emit(data), 0),
-    });
+	new Server({
+		close: () => undefined,
+		onClose: () => undefined,
+		onMessage: (cb) => {
+			c2s.event((d) => cb(d));
+		},
+		send: (data) => setTimeout(() => s2c.emit(data), 0),
+	});
 
-    const client = new Client({
-        close: () => undefined,
-        onClose: () => undefined,
-        onMessage: (cb) => {
-            s2c.event((d) => cb(d));
-        },
-        send: (data) => setTimeout(() => c2s.emit(data), 0),
-    });
+	const client = new Client({
+		close: () => undefined,
+		onClose: () => undefined,
+		onMessage: (cb) => {
+			s2c.event((d) => cb(d));
+		},
+		send: (data) => setTimeout(() => c2s.emit(data), 0),
+	});
 
-    return client;
+	return client;
 };
