a353a59945d02eded39c74e1493ef29ffc35b68d
Asher <ash@coder.com>
2019-02-01 12:38:42 -0600

    Move wheel event change out of patch

--- packages/vscode/src/fill/dom.ts
+++ packages/vscode/src/fill/dom.ts
@@ -1,3 +1,6 @@
+import * as dom from "vs/base/browser/dom";
+import { IDisposable } from "vs/base/common/lifecycle";
+
 // Firefox has no implementation of toElement.
 if (!("toElement" in MouseEvent.prototype)) {
 	Object.defineProperty(MouseEvent.prototype, "toElement", {
@@ -17,3 +20,11 @@ if (!("toElement" in MouseEvent.prototype)) {
 		},
 	});
 }
+
+const _addDisposableListener = dom.addDisposableListener;
+const addDisposableListener = (node: Element | Window | Document, type: string, handler: (event: any) => void, useCapture?: boolean): IDisposable => {
+	return _addDisposableListener(node, type === "mousewheel" ? "wheel" : type, handler, useCapture);
+};
+
+const target = dom as typeof dom;
+target.addDisposableListener = addDisposableListener;
