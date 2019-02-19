6708c8a5c80bd7f49295741932a2fdd7fe5160b5
Asher <ash@coder.com>
2019-02-19 17:33:22 -0600

    Prevent shared process from loading client code
    I think this happened when I added the trash code, since that's what
    includes the client.

--- packages/vscode/src/fill/stdioElectron.ts
+++ packages/vscode/src/fill/stdioElectron.ts
@@ -1,7 +1,12 @@
 import { StdioIpcHandler } from "@coder/server/src/ipc";
 import { IpcRenderer } from "electron";
 
-export * from "@coder/ide/src/fill/electron";
+// TODO: Commenting out for now since the electron fill includes the client code
+// and tries to connect to the web socket. The fill also likely wouldn't work
+// since it assumes it is running on the client. Could we proxy all methods to
+// the client? It might not matter since we intercept everything before sending
+// to the shared process.
+// export * from "@coder/ide/src/fill/electron";
 
 class StdioIpcRenderer extends StdioIpcHandler implements IpcRenderer {
 	// tslint:disable-next-line no-any
