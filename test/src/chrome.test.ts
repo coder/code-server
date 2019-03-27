4cc8209d30b2785a1bd60b2867917f61630ef925
Forest Hoffman <fhoffman@coder.com>
2019-03-27 18:42:42 -0500

    Add file creation test

--- test/src/chrome.test.ts
+++ test/src/chrome.test.ts
@@ -7,14 +7,14 @@ describe("chrome e2e", () => {
 	});
 	beforeEach(async () => {
 		await server.newPage();
+		await server.loadPage();
 	});
 	afterAll(async () => {
 		await server.dispose();
 	});
 
 	server.test("should open IDE", async () => {
-		const page = await server.loadPage();
-		const ideExists = await page.evaluate(() => {
+		const ideExists = await server.page.evaluate(() => {
 			return new Promise<boolean>((res, rej): void => {
 				const editor = document.querySelector("div.part.editor");
 				if (!editor) {
@@ -26,5 +26,10 @@ describe("chrome e2e", () => {
 			});
 		});
 		expect(ideExists).toEqual(true);
+	}, 2000);
+
+	server.test("should create file", async () => {
+		await server.page.hover("div.panel-header[aria-label='Files Explorer Section']");
+		await server.page.click("a.action-label.icon.new-file");
 	}, 2000);
 });
