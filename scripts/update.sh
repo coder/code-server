56d9f219c187876d9326a0d9119570a3d1f7fdc7
Illia Poplawski <illia.poplawski@gmail.com>
2019-04-09 18:59:15 -0400

    Update update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,5 +3,4 @@
 # Updates Code Server to the latest version
 curl -s https://api.github.com/repos/codercom/code-server/releases/latest | grep "browser_download_url.*code-server.*linux-x64.tar.gz" | cut -d '"' -f 4 | wget -O ~/code-server-linux.tar.gz --show-progress -qi -
 mkdir -p ~/code-server-linux && tar -xzvf ~/code-server-linux.tar.gz --strip 1 -C "$_"
-cd ~/code-server-linux || return
-chmod +x code-server
+chmod +x ~/code-server-linux/code-server
