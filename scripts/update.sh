0755e28699576feb3d770c362b1c416fd106cd19
Illia Poplawski <illia.poplawski@gmail.com>
2019-04-10 16:11:17 -0400

    Make update script OS aware for mac and linux

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+
 # Updates Code Server to the latest version
-curl -s https://api.github.com/repos/codercom/code-server/releases/latest | grep "browser_download_url.*code-server.*linux-x64.tar.gz" | cut -d '"' -f 4 | wget -O ~/code-server-linux.tar.gz --show-progress -qi -
-mkdir -p ~/code-server-linux && tar -xzvf ~/code-server-linux.tar.gz --strip 1 -C "$_"
-chmod +x ~/code-server-linux/code-server
+case $(uname -s) in
+  Darwin) # Mac
+    curl -s https://api.github.com/repos/codercom/code-server/releases/latest | \
+    grep "browser_download_url.*code-server.*darwin-x64.zip" | cut -d '"' -f 4 | \
+    awk '{print "url = "$1""}' | curl --create-dirs -o ~/code-server-darwin.zip --progress-bar -LK -
+    mkdir -p ~/code-server-darwin && tar -C "$_" xpvf ~/code-server-darwin.zip
+    chmod +x ~/code-server-darwin/code-server
+    ;;
+  Linux)
+    curl -s https://api.github.com/repos/codercom/code-server/releases/latest | \
+    grep "browser_download_url.*code-server.*linux-x64.tar.gz" | cut -d '"' -f 4 | \
+    wget -O ~/code-server-linux.tar.gz --show-progress -qi -
+    mkdir -p ~/code-server-linux && tar -xzvf ~/code-server-linux.tar.gz --strip 1 -C "$_"
+    chmod +x ~/code-server-linux/code-server
+    ;;
+  *)
+    echo "Unsupported operating system"
+    ;;
+esac
