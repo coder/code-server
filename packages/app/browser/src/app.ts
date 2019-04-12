db57aa229fb62bac87b6b941df1a92e822caf257
John McCambridge <jmccamb@gmail.com>
2019-04-12 10:35:30 -0500

    Allow use of the enter key for password input for code-server (#479)
    * Allow use of the enter key for password input for code-server
    
    * Remove function, make html form
    
    * Remove function and create html form
    
    * Handle form submit action
    
    * Remove button listener
    
    * Check if form exists

--- packages/app/browser/src/app.ts
+++ packages/app/browser/src/app.ts
@@ -20,11 +20,14 @@ window.addEventListener("message", (event) => {
 });
 
 const password = document.getElementById("password") as HTMLInputElement;
-const submit = document.getElementById("submit") as HTMLButtonElement;
-if (!submit) {
-	throw new Error("No submit button found");
+const form = document.getElementById("login-form") as HTMLFormElement;
+
+if (!form) {
+	throw new Error("No password form found");
 }
-submit.addEventListener("click", () => {
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
 	document.cookie = `password=${password.value}`;
 	location.reload();
 });
