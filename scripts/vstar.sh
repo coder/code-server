742dd6f597850395957733dba81df42194b5cf58
Asher <ash@coder.com>
2019-04-12 16:10:45 -0500

    Upgrade VS Code to 1.33.1

--- scripts/vstar.sh
+++ scripts/vstar.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # Builds a tarfile containing vscode sourcefiles neccessary for CI.
 # Done outside the CI and uploaded to object storage to reduce CI time.
 
-branch=1.33.0
+branch=1.33.1
 dir=/tmp/vstar
 outfile=/tmp/vstar-$branch.tar.gz
 rm -rf $dir
