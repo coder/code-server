4bff9f9f0d2304eb26e70d1dac4adf987926c0ff
John McCambridge <jmccamb@gmail.com>
2019-04-25 16:39:17 -0500

    Dispose of non-working command pallet actions

--- packages/vscode/src/fill/workbenchRegistry.ts
+++ packages/vscode/src/fill/workbenchRegistry.ts
@@ -1,4 +1,3 @@
-import { logger } from "@coder/logger";
 import { IDisposable } from "vs/base/common/lifecycle";
 import { Registry } from "vs/platform/registry/common/platform";
 import { IWorkbenchActionRegistry, Extensions } from "vs/workbench/common/actions";
@@ -6,23 +5,37 @@ import { SyncActionDescriptor } from "vs/platform/actions/common/actions";
 import { ContextKeyExpr } from "vs/platform/contextkey/common/contextkey";
 import { ToggleDevToolsAction } from "vs/workbench/electron-browser/actions/developerActions";
 import { TerminalPasteAction } from "vs/workbench/contrib/terminal/browser/terminalActions";
+import { CloseCurrentWindowAction, NewWindowAction, ShowAboutDialogAction, ZoomInAction, ZoomOutAction, ZoomResetAction } from "vs/workbench/electron-browser/actions/windowActions";
+import { REVEAL_IN_OS_COMMAND_ID } from "vs/workbench/contrib/files/browser/fileCommands";
+import { OpenProcessExplorer } from "vs/workbench/contrib/issue/electron-browser/issueActions";
+import { OpenPrivacyStatementUrlAction, OpenRequestFeatureUrlAction, OpenTwitterUrlAction } from "vs/workbench/electron-browser/actions/helpActions";
+import { CloseWorkspaceAction } from "vs/workbench/browser/actions/workspaceActions";
 import { KEYBINDING_CONTEXT_TERMINAL_FOCUS } from "vs/workbench/contrib/terminal/common/terminal";
 import { KeyCode, KeyMod } from "vs/base/common/keyCodes";
 import { workbench } from "../workbench";
 
 // Intercept adding workbench actions so we can skip actions that won't work or
 // modify actions that need different conditions, keybindings, etc.
+const toSkip = [
+	ToggleDevToolsAction.ID,
+	OpenTwitterUrlAction.ID,
+	OpenPrivacyStatementUrlAction.ID,
+	ShowAboutDialogAction.ID,
+	ZoomInAction.ID,
+	ZoomOutAction.ID,
+	ZoomResetAction.ID,
+	OpenProcessExplorer.ID,
+	OpenRequestFeatureUrlAction.ID,
+	NewWindowAction.ID,
+	CloseCurrentWindowAction.ID,
+	CloseWorkspaceAction.ID,
+	REVEAL_IN_OS_COMMAND_ID,
+];
+
 const registry = Registry.as<IWorkbenchActionRegistry>(Extensions.WorkbenchActions);
 const originalRegister = registry.registerWorkbenchAction.bind(registry);
 registry.registerWorkbenchAction = (descriptor: SyncActionDescriptor, alias: string, category?: string, when?: ContextKeyExpr): IDisposable => {
 	switch (descriptor.id) {
-		case ToggleDevToolsAction.ID: // There appears to be no way to toggle this programmatically.
-			logger.debug(`Skipping unsupported workbench action ${descriptor.id}`);
-
-			return {
-				dispose: (): void => undefined,
-			};
-
 		case TerminalPasteAction.ID: // Modify the Windows keybinding and add our context key.
 			// tslint:disable-next-line no-any override private
 			(descriptor as any)._keybindings = {
@@ -33,6 +46,15 @@ registry.registerWorkbenchAction = (descriptor: SyncActionDescriptor, alias: str
 			};
 			// tslint:disable-next-line no-any override private
 			(descriptor as any)._keybindingContext = ContextKeyExpr.and(KEYBINDING_CONTEXT_TERMINAL_FOCUS, workbench.clipboardContextKey);
+
+		default: // Dispose of non-working actions
+			for (let i = 0; i < toSkip.length; i++) {
+				if (descriptor.id === toSkip[i]) {
+					return {
+						dispose: (): void => undefined,
+					};
+				}
+			}
 	}
 
 	return originalRegister(descriptor, alias, category, when);
