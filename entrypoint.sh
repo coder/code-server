08ca5beaf2c4081de97e7d9a811419849eff6de9
satlus <aaron@satlow.com>
2019-04-26 15:40:40 -0400

    fixing bug in usermod -g

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [[ -z $CODER_UID || -z $CODER_GID ]];
     echo "didn't find CODER_UID or CODER_GID env vars, running with container default uid:gid"
  else 
     echo "found CODER_UID and CODER_GID env vars, running usermod...";
-    usermod -u $CODER_UID coder && sudo usermod -g $CODER_GID coder372
+    usermod -u $CODER_UID coder && sudo usermod -g $CODER_GID coder
 fi
 
 exec "$@"
