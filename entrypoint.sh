70424c8edcadb7f15f2890b38e9f3d01a5de2496
satlus <aaron@satlow.com>
2019-04-26 14:31:23 -0400

    There was an attempt.. it seems the usermod approach won't work
    Checking this non working code for the record. The idea is to usermod
    to change the uid:gid in entrypoint script. There are two approaches
    I took to do this:
    1. pass the -u flag to docker run and try to do the usermod. Won't work because we're not in the passwd file aand so user mod fails
    2. pass env vars with the host uid:gid you want to run as, and then usermod to change in-flight (fails, as we're trying to change the uid while logged in/running proc
    
    It seems eding the container /etc/passwd file is the only workable approach. One more thing to try is to run eentrypoint.sh before dumbinit - will do that next

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 export HOME=/home/coder
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
 
-sudo usermod -u $USER_ID coder && sudo groupmod -g $GROUP_ID coder
+if [[ -z $CODER_UID || -z $CODER_GID ]];
+ then 
+    echo "didn't find CODER_UID or CODER_GID env vars, running with container default uid:gid"
+ else 
+    echo "found CODER_UID and CODER_GID env vars, running usermod...";
+    usermod -u $CODER_UID coder && sudo usermod -g $CODER_GID coder372
+fi
 
 exec "$@"
