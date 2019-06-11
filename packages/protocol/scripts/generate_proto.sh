02f62882b8a9ecab98c20b6a3ffe0525f3d63caf
Anmol Sethi <hi@nhooyr.io>
2019-06-11 12:54:49 -0400

    Propagate full env to browser (#756)
    * Propogate full env to browser
    
    * Add support for setting $ITEM_URL
    
    * Add serviceURL getter

--- packages/protocol/scripts/generate_proto.sh
+++ packages/protocol/scripts/generate_proto.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cd "$(dirname "$0")/.."
+
 protoc --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" --js_out="import_style=commonjs,binary:./src/proto" --ts_out="./src/proto" ./src/proto/*.proto --proto_path="./src/proto"
