a16cef181416a68b72254e828d220bbb4d350470
Ayane Satomi <chinodesuuu@gmail.com>
2019-09-13 06:55:13 +0800

    [scripts/ci] add -C flag to prevent yum from updating
    See: https://unix.stackexchange.com/questions/74449/yum-install-package-without-updating-other-packages-or-fail
    
    Requires cdr/nbin#22

--- scripts/ci.bash
+++ scripts/ci.bash
@@ -21,7 +21,7 @@ function docker-build() {
 		docker exec "${containerId}" apk add libxkbfile-dev libsecret-dev
 	else
 		# TODO: at some point git existed but it seems to have disappeared.
-		docker exec "${containerId}" yum install -y libxkbfile-devel libsecret-devel git
+		docker exec "${containerId}" yum install -y libxkbfile-devel libsecret-devel git -C
 	fi
 
 	function docker-exec() {
