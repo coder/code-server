25f18beda4669cbf008cb42f169480406984f27f
Asher <ash@coder.com>
2020-01-16 18:09:19 -0600

    Fix version test

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,7 @@ main() {
 	version=$(./binaries/code-server* --version | head -1)
 	echo "Got '$version' for the version"
 	case $version in
-		*2.*-vsc1.41.1) exit 0 ;;
+		*-vsc1.41.1) exit 0 ;;
 		*) exit 1 ;;
 	esac
 }
