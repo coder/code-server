1a91588c424ec249f8c3c8aef783eea32c3cb6e3
Anmol Sethi <hi@nhooyr.io>
2020-02-18 13:06:18 -0500

    Add docker image pushing

--- ci/image/run.sh
+++ ci/image/run.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 main() {
   cd "$(dirname "$0")/../.."
+
+  docker build ci/image
   imageTag="$(docker build -q ci/image)"
   docker run -t --rm -e CI -e GITHUB_TOKEN -v "$(yarn cache dir):/usr/local/share/.cache/yarn/v6" -v "$PWD:/repo" -w /repo "$imageTag" "$*"
 }
