1aaa53622dea803776b3f44feedcff23ec606909
Anmol Sethi <hi@nhooyr.io>
2020-02-18 16:34:28 -0500

    Pass through travis tag in run.sh

--- ci/image/run.sh
+++ ci/image/run.sh
@@ -7,7 +7,7 @@ main() {
 
   docker build ci/image
   imageTag="$(docker build -q ci/image)"
-  docker run -t --rm -e CI -e GITHUB_TOKEN -v "$(yarn cache dir):/usr/local/share/.cache/yarn/v6" -v "$PWD:/repo" -w /repo "$imageTag" "$*"
+  docker run -t --rm -e CI -e GITHUB_TOKEN -e TRAVIS_TAG -v "$(yarn cache dir):/usr/local/share/.cache/yarn/v6" -v "$PWD:/repo" -w /repo "$imageTag" "$*"
 }
 
 main "$@"
