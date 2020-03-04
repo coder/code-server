a442d3e3f9fee66a7a8df7cce1124048df4391d0
Asher <ash@coder.com>
2020-03-04 15:22:32 -0600

    Make symlinking the entry script work

--- ci/code-server.sh
+++ ci/code-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # code-server.sh -- Run code-server with the bundled Node binary.
 
-cd "$(dirname "$0")" || exit 1
+cd "$(dirname "$(readlink -f "$0" || realpath "$0")")" || exit 1
 
 ./node ./out/node/entry.js "$@"
