8c47ba255aa634ebb23482ccd8f5f98a0ecf7525
Asher <ash@coder.com>
2020-03-05 10:26:14 -0600

    Preserve current working directory
    Fixes #1388.

--- ci/code-server.sh
+++ ci/code-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # code-server.sh -- Run code-server with the bundled Node binary.
 
-cd "$(dirname "$(readlink -f "$0" || realpath "$0")")" || exit 1
+dir="$(dirname "$(readlink -f "$0" || realpath "$0")")"
 
-./node ./out/node/entry.js "$@"
+"$dir/node" "$dir/out/node/entry.js" "$@"
