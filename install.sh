9a3b9fcac23e6bd7f01e46bae94ea7c017bbc0f7
Anmol Sethi <hi@nhooyr.io>
2020-06-02 17:22:24 -0400

    Fix install script to trim leading v from version
    Updates #1746

--- install.sh
+++ install.sh
@@ -62,7 +62,8 @@ EOF
 echo_latest_version() {
   # https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c#gistcomment-2758860
   version="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/cdr/code-server/releases/latest)"
-  version="${version#https://github.com/cdr/code-server/releases/tag/v}"
+  version="${version#https://github.com/cdr/code-server/releases/tag/}"
+  version="${version#v}"
   echo "$version"
 }
 
