5efe365ac71f8481bc5b8b762af26eb62ca54ce4
Siva <sivapvarma@users.noreply.github.com>
2020-08-28 04:31:38 -0700

    escape $ inside the systemctl doc string

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ echo_systemd_postinstall() {
   echoh
   cath << EOF
 To have systemd start code-server now and restart on boot:
-  sudo systemctl enable --now code-server@$USER
+  sudo systemctl enable --now code-server@\$USER
 Or, if you don't want/need a background service you can run:
   code-server
 EOF
