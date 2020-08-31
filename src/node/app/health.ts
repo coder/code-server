54ddd8bbf875b9964652470f3a83fc05695e9851
Jacob Goldman <jacobmgoldman@icloud.com>
2020-08-31 07:37:03 -0700

    linter changes and refactored alive() to Heart

--- src/node/app/health.ts
+++ src/node/app/health.ts
@@ -6,38 +6,27 @@ import { HttpProvider, HttpResponse, Route, Heart, HttpProviderOptions } from ".
  * Check the heartbeat.
  */
 export class HealthHttpProvider extends HttpProvider {
+  public constructor(options: HttpProviderOptions, private readonly heart: Heart) {
+    super(options)
+  }
 
-    public constructor(
-        options: HttpProviderOptions,
-        private readonly heart: Heart
-      ) {
-        super(options)
+  public async handleRequest(route: Route, request: http.IncomingMessage): Promise<HttpResponse> {
+    if (!this.authenticated(request)) {
+      if (this.isRoot(route)) {
+        return { redirect: "/login", query: { to: route.fullPath } }
+      }
+      throw new HttpError("Unauthorized", HttpCode.Unauthorized)
     }
 
-    private alive(): Boolean {
-        const now = Date.now()
-        return (now - this.heart.lastHeartbeat < this.heart.heartbeatInterval)
+    const result = {
+      cache: false,
+      mime: "application/json",
+      content: {
+        status: this.heart.alive() ? "alive" : "expired",
+        lastHeartbeat: this.heart.lastHeartbeat,
+      },
     }
 
-    public async handleRequest(route: Route, request: http.IncomingMessage): Promise<HttpResponse> {
-        if (!this.authenticated(request)) {
-            if (this.isRoot(route)) {
-            return { redirect: "/login", query: { to: route.fullPath } }
-            }
-            throw new HttpError("Unauthorized", HttpCode.Unauthorized)
-        }
-
-        const result = {
-            cache: false,
-            mime: 'application/json',
-            content: {
-                status: (this.alive()) ? 'alive' : 'expired',
-                lastHeartbeat: this.heart.lastHeartbeat
-
-            }
-        }
-
-        return result
-        
-    }
+    return result
+  }
 }
