4e2acab0b1a91a99150ffc622fd1b8cd812a9c77
Anmol Sethi <hi@nhooyr.io>
2020-09-03 15:41:42 -0400

    docker: Fix $DOCKER_USER
    We do not try renaming $HOME anymore as there is no good way
    to do it.
    
    We also only try to convert if the user hasn't been changed.
    
    Finally I added usage to the docker docs in install.md
    
    Closes #2056

--- ci/release-image/entrypoint.sh
+++ ci/release-image/entrypoint.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -eu
-
-if [ "${DOCKER_USER-}" ]; then
-  echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
-  sudo usermod --login "$DOCKER_USER" \
-    --move-home --home "/home/$DOCKER_USER" \
-    coder
-  sudo groupmod -n "$DOCKER_USER" coder
-
-  sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
-  sudo sed -i "s/coder/$DOCKER_USER/g" /etc/fixuid/config.yml
-  export HOME="/home/$DOCKER_USER"
-fi
 
 # This isn't set by default.
 export USER="$(whoami)"
+
+if [ "${DOCKER_USER-}" != "$USER" ]; then
+  echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
+  # Unfortunately we cannot change $HOME as we cannot move any bind mounts
+  # nor can we bind mount $HOME into a new home as that requires a privileged container.
+  sudo usermod --login "$DOCKER_USER" coder
+  sudo groupmod -n "$DOCKER_USER" coder
+
+  export USER="$(whoami)"
+
+  sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
+  sudo sed -i "s/coder/$DOCKER_USER/g" /etc/fixuid/config.yml
+fi
+
 dumb-init fixuid -q /usr/bin/code-server "$@"
