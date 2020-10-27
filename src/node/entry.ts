504d89638b4b55a62ffc65144cce6a140aedcde5
Asher <ash@coder.com>
2020-10-27 17:41:11 -0500

    Fix open line being printed when open fails
    Opening the URL can fail if the user doesn't have something appropriate
    installed to handle it.

--- src/node/entry.ts
+++ src/node/entry.ts
@@ -162,10 +162,12 @@ const main = async (args: DefaultedArgs): Promise<void> => {
   if (!args.socket && args.open) {
     // The web socket doesn't seem to work if browsing with 0.0.0.0.
     const openAddress = serverAddress.replace("://0.0.0.0", "://localhost")
-    await open(openAddress).catch((error: Error) => {
+    try {
+      await open(openAddress)
+      logger.info(`Opened ${openAddress}`)
+    } catch (error) {
       logger.error("Failed to open", field("address", openAddress), field("error", error))
-    })
-    logger.info(`Opened ${openAddress}`)
+    }
   }
 }
 
