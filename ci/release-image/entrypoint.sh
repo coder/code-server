a2ab0461d281533fc31c7d28e3488506657f7ca8
Anmol Sethi <hi@nhooyr.io>
2020-12-14 09:53:18 -0500

    release-image: Ignore $DOCKER_USER on restart
    Closes #2267

--- ci/release-image/entrypoint.sh
+++ ci/release-image/entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 # Otherwise the current container UID may not exist in the passwd database.
 eval "$(fixuid -q)"
 
-if [ "${DOCKER_USER-}" ]; then
+if [ "${DOCKER_USER-}" ] && [ "$DOCKER_USER" != "$USER" ]; then
   echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
   # Unfortunately we cannot change $HOME as we cannot move any bind mounts
   # nor can we bind mount $HOME into a new home as that requires a privileged container.
