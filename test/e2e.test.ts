6685a3e364f0e37d5f968de4991d31de8e6a0e8f
Joe Previte <jjprevite@gmail.com>
2021-01-27 16:04:37 -0700

    feat: update workflow

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -2,6 +2,7 @@ import { chromium, Page, Browser } from "playwright"
 
 let browser: Browser
 let page: Page
+
 beforeAll(async () => {
   browser = await chromium.launch()
 })
@@ -15,7 +16,8 @@ afterEach(async () => {
   await page.close()
 })
 
-it("should work", async () => {
-  await page.goto("https://www.example.com/")
-  expect(await page.title()).toBe("Example Domain")
+it("should see the login page", async () => {
+  await page.goto("http://localhost:8080")
+  // It should send us to the login page
+  expect(await page.title()).toBe("code-server login")
 })
