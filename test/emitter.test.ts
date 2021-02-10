325a1d8181415459a3531745bae43446252498a7
Joe Previte <jjprevite@gmail.com>
2021-02-10 15:37:43 -0700

    feat: add test for catching errors in Emitter

--- test/emitter.test.ts
+++ test/emitter.test.ts
@@ -1,52 +1,85 @@
+// Note: we need to import logger from the root
+// because this is the logger used in logError in ../src/common/util
+import { logger } from "../node_modules/@coder/logger"
 import { Emitter } from "../src/common/emitter"
 
-describe("emitter", () => {
-  describe("Emitter", () => {
-    it("should return an Emitter", async () => {
-      const HELLO_WORLD = "HELLO_WORLD"
-      const GOODBYE_WORLD = "GOODBYE_WORLD"
-      const mockCallback = jest.fn(() => "Mock function called")
-      const mockSecondCallback = jest.fn(() => undefined)
+describe("Emitter", () => {
+  let spy: jest.SpyInstance
 
-      const emitter = new Emitter<{ event: string; callback: () => void }>()
+  beforeEach(() => {
+    spy = jest.spyOn(logger, "error")
+  })
 
-      const onHelloWorld = ({ event, callback }: { event: string; callback: () => void }): void => {
-        if (event === HELLO_WORLD) {
-          callback()
-        }
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it("should run the correct callbacks", async () => {
+    const HELLO_WORLD = "HELLO_WORLD"
+    const GOODBYE_WORLD = "GOODBYE_WORLD"
+    const mockCallback = jest.fn(() => "Mock function called")
+    const mockSecondCallback = jest.fn(() => undefined)
+
+    const emitter = new Emitter<{ event: string; callback: () => void }>()
+
+    const onHelloWorld = ({ event, callback }: { event: string; callback: () => void }): void => {
+      if (event === HELLO_WORLD) {
+        callback()
       }
+    }
 
-      const onGoodbyeWorld = ({ event, callback }: { event: string; callback: () => void }): void => {
-        if (event === GOODBYE_WORLD) {
-          callback()
-        }
+    const onGoodbyeWorld = ({ event, callback }: { event: string; callback: () => void }): void => {
+      if (event === GOODBYE_WORLD) {
+        callback()
       }
+    }
 
-      // Register the onHelloWorld listener
-      // and the onGoodbyeWorld
-      emitter.event(onHelloWorld)
-      emitter.event(onGoodbyeWorld)
+    // Register the onHelloWorld listener
+    // and the onGoodbyeWorld
+    emitter.event(onHelloWorld)
+    emitter.event(onGoodbyeWorld)
 
-      await emitter.emit({ event: HELLO_WORLD, callback: mockCallback })
+    await emitter.emit({ event: HELLO_WORLD, callback: mockCallback })
 
-      // Double-check that our callback is called only once
-      expect(mockCallback).toHaveBeenCalled()
-      expect(mockCallback).toHaveBeenCalledTimes(1)
+    // Double-check that our callback is called only once
+    expect(mockCallback).toHaveBeenCalled()
+    expect(mockCallback).toHaveBeenCalledTimes(1)
 
-      await emitter.emit({ event: GOODBYE_WORLD, callback: mockSecondCallback })
+    await emitter.emit({ event: GOODBYE_WORLD, callback: mockSecondCallback })
 
-      // Check that it works with multiple listeners
-      expect(mockSecondCallback).toHaveBeenCalled()
-      expect(mockSecondCallback).toHaveBeenCalledTimes(1)
+    // Check that it works with multiple listeners
+    expect(mockSecondCallback).toHaveBeenCalled()
+    expect(mockSecondCallback).toHaveBeenCalledTimes(1)
 
-      // Dispose of all the listeners
-      emitter.dispose()
-    })
+    // Dispose of all the listeners
+    emitter.dispose()
+  })
 
-    it.skip("should log an error if something goes wrong", () => {
-      // not sure how we're going to test this
-      // need to mock logger
-      // and then somehow throw or something in the callback
-    })
+  it("should log an error if something goes wrong", async () => {
+    const HELLO_WORLD = "HELLO_WORLD"
+    const mockCallback = jest.fn(() => "Mock function called")
+    const message = "You don't have access to that folder."
+
+    const emitter = new Emitter<{ event: string; callback: () => void }>()
+
+    const onHelloWorld = ({ event, callback }: { event: string; callback: () => void }): void => {
+      if (event === HELLO_WORLD) {
+        callback()
+        throw new Error(message)
+      }
+    }
+
+    emitter.event(onHelloWorld)
+
+    await emitter.emit({ event: HELLO_WORLD, callback: mockCallback })
+
+    // Check that error was called
+    expect(spy).toHaveBeenCalled()
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(message)
   })
 })
