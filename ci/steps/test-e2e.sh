9ee2556dd1ca199d46c6250d4eeacdbe6548daa6
Joe Previte <jjprevite@gmail.com>
2021-03-09 16:36:56 -0700

    chore: update gitignore with test dirs

--- ci/steps/test-e2e.sh
+++ ci/steps/test-e2e.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 main() {
   cd "$(dirname "$0")/../.."
 
-  ./release-packages/code-server*-linux-amd64/bin/code-server --home $CODE_SERVER_ADDRESS/healthz &
+  "./release-packages/code-server*-linux-amd64/bin/code-server" --home "$CODE_SERVER_ADDRESS"/healthz &
   yarn --frozen-lockfile
   yarn test:e2e
 }
