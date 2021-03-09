6fd30d91eabcfdeebc701430bdce168e0c3bca28
Joe Previte <jjprevite@gmail.com>
2021-03-09 16:35:34 -0700

    feat: add jest e2e config

---
+++ test/jest.e2e.config.ts
@@ -0,0 +1,22 @@
+// jest.config.ts
+import type { Config } from "@jest/types"
+
+const config: Config.InitialOptions = {
+  transform: {
+    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest",
+  },
+  globalSetup: "<rootDir>/utils/globalSetup.ts",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["node_modules", "lib", "out", "test/unit"],
+  testTimeout: 30000,
+  modulePathIgnorePatterns: [
+    "<rootDir>/../lib/vscode",
+    "<rootDir>/../release-packages",
+    "<rootDir>/../release",
+    "<rootDir>/../release-standalone",
+    "<rootDir>/../release-npm-package",
+    "<rootDir>/../release-gcp",
+    "<rootDir>/../release-images",
+  ],
+}
+export default config
