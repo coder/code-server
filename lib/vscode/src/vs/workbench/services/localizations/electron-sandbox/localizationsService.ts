8512be85a552adea7bd48db8ee7d584f469c11d0
Joe Previte <jjprevite@gmail.com>
2021-03-11 11:09:37 -0700

    fix(lib/vscode): update electron-sandbox localizationService
    I'm not sure if we even use this but they changed the LocalizationService.o
    
    It looks like it using one called ISharedProcessService now.

--- lib/vscode/src/vs/workbench/services/localizations/electron-sandbox/localizationsService.ts
+++ lib/vscode/src/vs/workbench/services/localizations/electron-sandbox/localizationsService.ts
@@ -5,12 +5,8 @@
 
 import { ProxyChannel } from 'vs/base/parts/ipc/common/ipc';
 import { ILocalizationsService } from 'vs/platform/localizations/common/localizations';
-<<<<<<< HEAD:lib/vscode/src/vs/workbench/services/localizations/electron-browser/localizationsService.ts
-=======
 import { ISharedProcessService } from 'vs/platform/ipc/electron-sandbox/services';
->>>>>>> e8cd17a97d8c58fffcbac05394b3ee2b3c72d384:lib/vscode/src/vs/workbench/services/localizations/electron-sandbox/localizationsService.ts
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
-import { IRemoteAgentService } from 'vs/workbench/services/remote/common/remoteAgentService';
 
 // @ts-ignore: interface is implemented via proxy
 export class LocalizationsService implements ILocalizationsService {
@@ -18,13 +14,9 @@ export class LocalizationsService implements ILocalizationsService {
 	declare readonly _serviceBrand: undefined;
 
 	constructor(
-		@IRemoteAgentService remoteAgentService: IRemoteAgentService,
+		@ISharedProcessService sharedProcessService: ISharedProcessService,
 	) {
-<<<<<<< HEAD:lib/vscode/src/vs/workbench/services/localizations/electron-browser/localizationsService.ts
-		return createChannelSender<ILocalizationsService>(remoteAgentService.getConnection()!.getChannel('localizations'));
-=======
 		return ProxyChannel.toService<ILocalizationsService>(sharedProcessService.getChannel('localizations'));
->>>>>>> e8cd17a97d8c58fffcbac05394b3ee2b3c72d384:lib/vscode/src/vs/workbench/services/localizations/electron-sandbox/localizationsService.ts
 	}
 }
 
