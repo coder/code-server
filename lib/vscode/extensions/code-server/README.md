060aeacce5bdc461759a9a1e614e30310b97adc2
Asher <ash@coder.com>
2021-04-16 16:42:18 -0500

    Add builtin plugin for code-server integration

---
+++ lib/vscode/extensions/code-server/README.md
@@ -0,0 +1,3 @@
+# code-server integration for Visual Studio Code
+
+**Notice:** This extension is bundled with Visual Studio Code. It can be disabled but not uninstalled.
---
+++ lib/vscode/extensions/code-server/src/main.ts
@@ -0,0 +1,8 @@
+import { ExtensionContext } from 'vscode';
+
+// import * as nls from 'vscode-nls';
+// const localize = nls.loadMessageBundle();
+
+export async function activate(context: ExtensionContext): Promise<void> {
+	console.log('ACTIVATED');
+}
