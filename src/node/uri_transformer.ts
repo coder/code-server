8c3c30a3c06c4ff237802303f159999c25f6d65b
Akash Satheesan <akash@coder.com>
2021-06-10 23:16:44 +0530

    chore: format file

--- src/node/uri_transformer.ts
+++ src/node/uri_transformer.ts
@@ -2,43 +2,49 @@
 // remember to update this for proper type checks!
 
 interface UriParts {
-	scheme: string;
-	authority?: string;
-	path?: string;
+  scheme: string
+  authority?: string
+  path?: string
 }
 
 interface IRawURITransformer {
-	transformIncoming(uri: UriParts): UriParts;
-	transformOutgoing(uri: UriParts): UriParts;
-	transformOutgoingScheme(scheme: string): string;
+  transformIncoming(uri: UriParts): UriParts
+  transformOutgoing(uri: UriParts): UriParts
+  transformOutgoingScheme(scheme: string): string
 }
 
 // This is deliberate! see src/vs/workbench/services/extensions/node/extensionHostProcessSetup.ts
 export = function rawURITransformerFactory(authority: string) {
-    return new RawURITransformer(authority);
+  return new RawURITransformer(authority)
 }
 
 class RawURITransformer implements IRawURITransformer {
-    constructor(private readonly authority: string) {}
+  constructor(private readonly authority: string) {}
 
-    transformIncoming(uri: UriParts): UriParts {
-        switch (uri.scheme) {
-            case "vscode-remote": return {scheme: "file", path: uri.path};
-            default: return uri;
-        }
+  transformIncoming(uri: UriParts): UriParts {
+    switch (uri.scheme) {
+      case "vscode-remote":
+        return { scheme: "file", path: uri.path }
+      default:
+        return uri
     }
+  }
 
-    transformOutgoing(uri: UriParts): UriParts {
-        switch (uri.scheme) {
-            case "file": return {scheme: "vscode-remote", authority: this.authority, path: uri.path};
-            default: return uri;
-        }
+  transformOutgoing(uri: UriParts): UriParts {
+    switch (uri.scheme) {
+      case "file":
+        return { scheme: "vscode-remote", authority: this.authority, path: uri.path }
+      default:
+        return uri
     }
+  }
 
-    transformOutgoingScheme(scheme: string): string {
-        switch (scheme) {
-            case "file": return "vscode-remote";
-            default: return scheme; 
-        }
+  transformOutgoingScheme(scheme: string): string {
+    switch (scheme) {
+      case "file":
+        return "vscode-remote"
+      default:
+        return scheme
     }
+  }
 }
