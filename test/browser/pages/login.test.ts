6e1455da88966b86adcf8423bc10b2880c5e2c8b
Joe Previte <jjprevite@gmail.com>
2021-06-23 10:49:22 -0700

    feat: add custom codecov

--- test/browser/pages/login.test.ts
+++ test/browser/pages/login.test.ts
@@ -25,10 +25,14 @@ describe("login", () => {
       // Create a fake element and set the attribute
       const mockElement = document.createElement("input")
       mockElement.setAttribute("id", "base")
-      mockElement.setAttribute(
-        "data-settings",
-        '{"base":"./hello-world","csStaticBase":"./static/development/Users/jp/Dev/code-server","logLevel":2,"disableTelemetry":false,"disableUpdateCheck":false}',
-      )
+      const expected = {
+        base: "./hello-world",
+        csStaticBase: "./static/development/Users/jp/Dev/code-server",
+        logLevel: 2,
+        disableTelemetry: false,
+        disableUpdateCheck: false,
+      }
+      mockElement.setAttribute("data-settings", JSON.stringify(expected))
       document.body.appendChild(mockElement)
       spy.mockImplementation(() => mockElement)
       // Load file
@@ -36,34 +40,6 @@ describe("login", () => {
 
       const el: HTMLInputElement | null = document.querySelector("input#base")
       expect(el?.value).toBe("/hello-world")
-    })
-  })
-
-  describe("there is no element with id 'base'", () => {
-    let initialDom: Document
-    beforeEach(() => {
-      const dom = new JSDOM()
-      initialDom = dom.window.document
-      global.document = dom.window.document
-
-      const location: LocationLike = {
-        pathname: "/healthz",
-        origin: "http://localhost:8080",
-      }
-
-      global.location = location as Location
-    })
-    afterEach(() => {
-      // Reset the global.document
-      global.document = undefined as any as Document
-      global.location = undefined as any as Location
-    })
-
-    it("should not change the DOM", () => {
-      // Load file
-      require("../../../src/browser/pages/login")
-      const currentDom = global.document
-      expect(initialDom).toBe(currentDom)
     })
   })
 })
