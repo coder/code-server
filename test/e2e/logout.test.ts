43c6ffcb8f18719bcb5b36c831a918387acf84db
Asher <ash@coder.com>
2021-06-25 11:20:44 -0500

    Remove login steps from logout test
    I figure login is already tested so we can skip this and just use the
    cookie.

--- test/e2e/logout.test.ts
+++ test/e2e/logout.test.ts
@@ -1,19 +1,7 @@
-import { PASSWORD } from "../utils/constants"
 import { describe, test, expect } from "./baseFixture"
 
-describe("logout", false, () => {
-  test("should be able login and logout", async ({ codeServerPage }) => {
-    // Type in password
-    await codeServerPage.page.fill(".password", PASSWORD)
-    // Click the submit button and login
-    await codeServerPage.page.click(".submit")
-    await codeServerPage.page.waitForLoadState("networkidle")
-    // We do this because occassionally code-server doesn't load on Firefox
-    // but loads if you reload once or twice
-    await codeServerPage.reloadUntilEditorIsReady()
-    // Make sure the editor actually loaded
-    expect(await codeServerPage.isEditorVisible()).toBe(true)
-
+describe("logout", true, () => {
+  test("should be able logout", async ({ codeServerPage }) => {
     // Click the Application menu
     await codeServerPage.page.click("[aria-label='Application Menu']")
 
