a32df56f9925dae139620eda67e59c8978889c3b
Asher <ash@coder.com>
2021-07-16 17:35:36 -0500

    Skip Docker if already pushed

--- ci/steps/push-docker-manifest.sh
+++ ci/steps/push-docker-manifest.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# See if this version already exists on Docker Hub.
+function version_exists() {
+  local output
+  output=$(curl --silent "https://index.docker.io/v1/repositories/codercom/code-server/tags/$VERSION")
+  if [[ $output == "Tag not found" ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
 # Import and push the Docker image for the provided arch.
 push() {
   local arch=$1
@@ -25,6 +36,11 @@ push() {
 main() {
   cd "$(dirname "$0")/../.."
   source ./ci/lib.sh
+
+  if version_exists; then
+    echo "$VERSION is already pushed"
+    return
+  fi
 
   download_artifact release-images ./release-images
   if [[ ${CI-} ]]; then
