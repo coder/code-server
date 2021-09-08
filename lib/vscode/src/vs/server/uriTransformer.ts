d53b4970e52f01afa6c45e43e0346ded342c3ef4
Teffen Ellis <teffen@nirri.us>
2021-09-08 14:55:42 -0400

    Remove subtree.

--- lib/vscode/src/vs/server/uriTransformer.ts
+++
@@ -1,1 +0,0 @@
-../../../../../src/node/uriTransformer.ts
--- lib/vscode/src/vs/server/util.ts
+++
@@ -1,6 +0,0 @@
-import { URITransformer } from 'vs/base/common/uriIpc';
-import rawURITransformerFactory = require('vs/server/uriTransformer');
-
-export const getUriTransformer = (remoteAuthority: string): URITransformer => {
-	return new URITransformer(rawURITransformerFactory(remoteAuthority));
-};
