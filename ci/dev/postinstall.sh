3c61d96d4fb63af50754027e032cac0bd32a0578
Teffen Ellis <teffen@nirri.us>
2021-09-16 17:52:51 -0400

    Bump vscode.
    - Fix issue where yarn lock cannot be updated in development.

--- ci/dev/postinstall.sh
+++ ci/dev/postinstall.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 main() {
   cd "$(dirname "$0")/../.."
 
-  echo 'Installing code-server test dependencies...'
+  echo "Installing code-server test dependencies..."
 
   cd test
   yarn install
   cd ..
 
   cd vendor
-  echo 'Installing vendor dependencies...'
+  echo "Installing vendor dependencies..."
 
   # * We install in 'modules' instead of 'node_modules' because VS Code's extensions
   # use a webpack config which cannot differentiate between its own node_modules
@@ -21,7 +21,14 @@ main() {
   # devDependencies are not needed, and that even git repo based packages are
   # assumed to be compiled. Because the default behavior for VS Code's `postinstall`
   # assumes we're also compiled, this needs to be ignored.
-  yarn install --modules-folder modules --ignore-scripts --frozen-lockfile
+
+  local args=(install --modules-folder modules --ignore-scripts)
+
+  if [[ ${CI-} ]]; then
+    args+=("--frozen-lockfile")
+  fi
+
+  yarn "${args[@]}"
 
   # Finally, run the vendor `postinstall`
   yarn run postinstall
