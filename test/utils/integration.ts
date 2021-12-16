2fbb43f1079095b08a285e9efa287fa55e421f27
Asher <ash@coder.com>
2021-12-16 22:16:50 +0000

    Use separate data directories for unit test instances
    Exactly as we do for the e2e tests.

--- test/utils/integration.ts
+++ test/utils/integration.ts
@@ -1,11 +1,27 @@
+import { promises as fs } from "fs"
+import * as path from "path"
 import { parse, parseConfigFile, setDefaults } from "../../src/node/cli"
 import { runCodeServer } from "../../src/node/main"
+import { workspaceDir } from "./constants"
+import { tmpdir } from "./helpers"
 import * as httpserver from "./httpserver"
 
 export async function setup(argv: string[], configFile?: string): Promise<httpserver.HttpServer> {
-  argv = ["--bind-addr=localhost:0", "--log=warn", ...argv]
+  // This will be used as the data directory to ensure instances do not bleed
+  // into each other.
+  const dir = await tmpdir(workspaceDir)
 
-  const cliArgs = parse(argv)
+  // VS Code complains if the logs dir is missing which spams the output.
+  // TODO: Does that mean we are not creating it when we should be?
+  await fs.mkdir(path.join(dir, "logs"))
+
+  const cliArgs = parse([
+    `--config=${path.join(dir, "config.yaml")}`,
+    `--user-data-dir=${dir}`,
+    "--bind-addr=localhost:0",
+    "--log=warn",
+    ...argv,
+  ])
   const configArgs = parseConfigFile(configFile || "", "test/integration.ts")
   const args = await setDefaults(cliArgs, configArgs)
 
