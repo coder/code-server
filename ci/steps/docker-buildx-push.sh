03c62242ee2a60d1eb61b46335916dd34a95b48e
Jonathan Yu <jonathan@coder.com>
2022-02-11 09:32:02 -0800

    chore: allow overwriting Docker images (#4850)
    Allow Docker images to be overwritten, which is required to update
    dependencies in the base image when the upstream vendor resolves
    security issues.

--- ci/steps/docker-buildx-push.sh
+++ ci/steps/docker-buildx-push.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# See if this version already exists on Docker Hub.
-function version_exists() {
-  local output
-  output=$(curl --silent "https://index.docker.io/v1/repositories/codercom/code-server/tags/$VERSION")
-  if [[ $output == "Tag not found" ]]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
 main() {
   cd "$(dirname "$0")/../.."
 
   # ci/lib.sh sets VERSION and provides download_artifact here
   source ./ci/lib.sh
-
-  if version_exists; then
-    echo "$VERSION is already pushed"
-    return
-  fi
 
   # Download the release-packages artifact
   download_artifact release-packages ./release-packages
