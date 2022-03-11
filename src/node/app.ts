1329a769a63481c4fee187cab3145b4888224ff9
Joe Previte <jjprevite@gmail.com>
2022-03-11 13:11:56 -0700

    Update src/node/app.ts
    Co-authored-by: Jonathan Yu <jonathan@coder.com>

--- src/node/app.ts
+++ src/node/app.ts
@@ -47,7 +47,7 @@ export const listen = async (server: http.Server, { host, port, socket, "socket-
   })
 
   // NOTE@jsjoeio: we need to chmod after the server is finished
-  // listening. Otherwise, teh socket may not have been created yet.
+  // listening. Otherwise, the socket may not have been created yet.
   if (socket && mode) {
     await fs.chmod(socket, mode).catch((err) => {
       util.logError(logger, "socket chmod", err)
