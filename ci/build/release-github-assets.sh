5afb26fb6076e52a76fe2780f2eab4769475d35e
Joe Previte <jjprevite@gmail.com>
2022-03-17 11:52:39 -0700

    fix(ci): correctly download npm/docker artifacts (#4995)
    * fix(ci): correctly download npm artifact
    
    * fixup! fix(ci): correctly download npm artifact
    
    * docs: update MAINTAINING
    
    * fixup! docs: update MAINTAINING
    
    * fixup! Merge branch 'main' into 4949-chore-fix-npm-workflow
    
    * chore: get ci to run
    
    * refactor: use vVERSION branch name instead of release
    
    * refactor: use new download artifact in docker workflow
    
    * refactor: clean up release-github-assets script
    
    * fixup: remove extra v
    
    * fixup! fixup: remove extra v

--- ci/build/release-github-assets.sh
+++ ci/build/release-github-assets.sh
@@ -9,6 +9,15 @@ set -euo pipefail
 main() {
   cd "$(dirname "$0")/../.."
   source ./ci/lib.sh
+  source ./ci/steps/steps-lib.sh
+
+  # NOTE@jsjoeio - only needed if we use the download_artifact
+  # because we talk to the GitHub API.
+  # Needed to use GitHub API
+  if ! is_env_var_set "GITHUB_TOKEN"; then
+    echo "GITHUB_TOKEN is not set. Cannot download npm release-packages without GitHub credentials."
+    exit 1
+  fi
 
   download_artifact release-packages ./release-packages
   local assets=(./release-packages/code-server*"$VERSION"*{.tar.gz,.deb,.rpm})
