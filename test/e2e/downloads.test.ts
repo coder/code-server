dd9a2c383b9f47cb7e184e447894938fe1921dcb
Joe Previte <jjprevite@gmail.com>
2022-03-31 13:00:28 -0700

    feat(e2e): add download test

---
+++ test/e2e/downloads.test.ts
@@ -0,0 +1,26 @@
+import * as path from "path"
+import { promises as fs } from "fs"
+import { clean } from "../utils/helpers"
+import { describe, test, expect } from "./baseFixture"
+
+describe("Downloads", true, [], {}, async () => {
+  const testName = "downloads"
+  test.beforeAll(async () => {
+    await clean(testName)
+  })
+
+  test("should see the 'Download...' option", async ({ codeServerPage }) => {
+    // Setup
+    const workspaceDir = await codeServerPage.workspaceDir
+    const tmpFilePath = path.join(workspaceDir, "unique-file.txt")
+    await fs.writeFile(tmpFilePath, "hello world")
+
+    // Action
+    const fileInExplorer = await codeServerPage.page.waitForSelector("text=unique-file.txt")
+    await fileInExplorer.click({
+      button: "right",
+    })
+
+    expect(await codeServerPage.page.isVisible("text=Download...")).toBe(true)
+  })
+})
