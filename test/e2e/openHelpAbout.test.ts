c8d8d084eae50d0a99b9fb6e483cd1bca897438e
Asher <ash@coder.com>
2022-03-31 20:05:02 +0000

    Change version test to look for specific version
    This will catch if we are not sending the actual version to the client.

--- test/e2e/openHelpAbout.test.ts
+++ test/e2e/openHelpAbout.test.ts
@@ -1,3 +1,4 @@
+import { version } from "../../src/node/constants"
 import { describe, test, expect } from "./baseFixture"
 
 describe("Open Help > About", true, [], {}, () => {
@@ -5,8 +6,12 @@ describe("Open Help > About", true, [], {}, () => {
     // Open using the menu.
     await codeServerPage.navigateMenus(["Help", "About"])
 
+    const isDevMode = process.env.VSCODE_DEV === "1"
+
     // Look for code-server info div.
-    const element = await codeServerPage.page.waitForSelector('div[role="dialog"] >> text=code-server')
+    const element = await codeServerPage.page.waitForSelector(
+      `div[role="dialog"] >> text=code-server: ${isDevMode ? "Unknown" : "v" + version}`,
+    )
     expect(element).not.toBeNull()
   })
 })
