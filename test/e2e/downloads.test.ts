384c924572766d9a20c9a032be89712c14ab9d64
Joe Previte <jjprevite@gmail.com>
2022-04-11 15:42:51 -0700

    fixup!: combine e2e tests

--- test/e2e/downloads.test.ts
+++ test/e2e/downloads.test.ts
@@ -24,3 +24,25 @@ describe("Downloads (enabled)", true, [], {}, async () => {
     expect(await codeServerPage.page.isVisible("text=Download...")).toBe(true)
   })
 })
+
+describe("Downloads (disabled)", true, ["--disable-file-downloads"], {}, async () => {
+  const testName = "downloads"
+  test.beforeAll(async () => {
+    await clean(testName)
+  })
+
+  test("should not see the 'Download...' option", async ({ codeServerPage }) => {
+    // Setup
+    const workspaceDir = await codeServerPage.workspaceDir
+    const tmpFilePath = path.join(workspaceDir, "unique-file.txt")
+    await fs.writeFile(tmpFilePath, "hello world")
+
+    // Action
+    const fileInExplorer = await codeServerPage.page.waitForSelector("text=unique-file.txt")
+    await fileInExplorer.click({
+      button: "right",
+    })
+
+    expect(await codeServerPage.page.isVisible("text=Download...")).toBe(false)
+  })
+})
