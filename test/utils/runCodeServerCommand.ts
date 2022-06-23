79f6060bb8ec0cc80ab554669ac557b3d1beb19c
Joe Previte <jjprevite@gmail.com>
2022-06-23 15:40:20 -0500

    refactor: use different default binary path

--- test/utils/runCodeServerCommand.ts
+++ test/utils/runCodeServerCommand.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process"
+import path from "path";
 import { promisify } from "util"
 
 /**
@@ -6,7 +7,7 @@ import { promisify } from "util"
  * A helper function for integration tests to run code-server commands.
  */
 export async function runCodeServerCommand(argv: string[]): Promise<{ stdout: string; stderr: string }> {
-  const CODE_SERVER_COMMAND = process.env.CODE_SERVER_PATH || "/var/tmp/coder/code-server/bin/code-server"
+  const CODE_SERVER_COMMAND = process.env.CODE_SERVER_PATH || path.resolve("../../release-standalone/bin/code-server")
   const { stdout, stderr } = await promisify(exec)(`${CODE_SERVER_COMMAND} ${argv.join(" ")}`)
 
   return { stdout, stderr }
