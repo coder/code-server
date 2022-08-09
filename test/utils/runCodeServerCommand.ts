11b2ef9846e834cbe54e78a68e7ddb5350581e65
Joe Previte <jjprevite@gmail.com>
2022-08-09 15:41:37 -0500

    feat: add test for EXTENSIONS_GALLERY (#5432)
    * refactor: add env arg to runCodeServerCommand
    
    This allows yous to pass environment variables to code-server's helper
    when running integration tests.
    
    * feat: add EXTENSIONS_GALLERY integration test
    
    This test ensures EXTENSIONS_GALLERY is read when set and using the
    `--install-extension` flag.
    
    Co-authored-by: repo-ranger[bot] <39074581+repo-ranger[bot]@users.noreply.github.com>

--- test/utils/runCodeServerCommand.ts
+++ test/utils/runCodeServerCommand.ts
@@ -6,9 +6,14 @@ import { promisify } from "util"
  *
  * A helper function for integration tests to run code-server commands.
  */
-export async function runCodeServerCommand(argv: string[]): Promise<{ stdout: string; stderr: string }> {
+export async function runCodeServerCommand(
+  argv: string[],
+  env?: NodeJS.ProcessEnv,
+): Promise<{ stdout: string; stderr: string }> {
   const CODE_SERVER_COMMAND = process.env.CODE_SERVER_PATH || path.resolve("../../release-standalone/bin/code-server")
-  const { stdout, stderr } = await promisify(exec)(`${CODE_SERVER_COMMAND} ${argv.join(" ")}`)
+  const { stdout, stderr } = await promisify(exec)(`${CODE_SERVER_COMMAND} ${argv.join(" ")}`, {
+    env: { ...process.env, ...env },
+  })
 
   return { stdout, stderr }
 }
