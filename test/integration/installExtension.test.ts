c69f2c69f6505168acc1a1549c8242b51003a4fc
Joe Previte <jjprevite@gmail.com>
2022-08-10 17:59:36 -0500

    chore(testing): remove install/list tests (#5441)
    Now that we have a test for `--help` which checks to make sure native
    modules are working as expected, we don't need the `--install-extension`
    test or the `--list-extensions` test.
    
    We can also remove the `.vsix` fixture since we're not using that
    either.

--- test/integration/installExtension.test.ts
+++ test/integration/installExtension.test.ts
@@ -1,5 +1,3 @@
-import { stat } from "fs/promises"
-import path from "path"
 import { clean, tmpdir } from "../utils/helpers"
 import { runCodeServerCommand } from "../utils/runCodeServerCommand"
 
@@ -13,15 +11,6 @@ describe("--install-extension", () => {
     tempDir = await tmpdir(testName)
     setupFlags = ["--extensions-dir", tempDir]
   })
-  it("should install an extension", async () => {
-    const extName = `wesbos.theme-cobalt2-2.1.6`
-    const vsixFileName = "wesbos.theme-cobalt2-2.1.6.vsix"
-    const extensionFixture = path.resolve(`./test/integration/fixtures/${vsixFileName}`)
-    await runCodeServerCommand([...setupFlags, "--install-extension", extensionFixture])
-    const pathToExtFolder = path.join(tempDir, extName)
-    const statInfo = await stat(pathToExtFolder)
-    expect(statInfo.isDirectory()).toBe(true)
-  }, 20000)
   it("should use EXTENSIONS_GALLERY when set", async () => {
     const extName = `author.extension-1.0.0`
     const { stderr } = await runCodeServerCommand([...setupFlags, "--install-extension", extName], {
