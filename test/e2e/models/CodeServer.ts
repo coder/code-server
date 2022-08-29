a15ce70893cd8825612b1be97918673ac322559d
Joe Previte <jjprevite@gmail.com>
2022-08-29 11:07:57 -0700

    feat(e2e): add language extension to setup
    This adds a slimmed-down version of the Spanish Language Pack and also
    adds a `languagepacks.json` to the e2e `workspaceDir` which allows use
    to run a test suite passing the `--locale es` flags to simulate a
    different display language.

--- test/e2e/models/CodeServer.ts
+++ test/e2e/models/CodeServer.ts
@@ -88,6 +88,13 @@ export class CodeServer {
       }),
       "utf8",
     )
+
+    const extensionsDir = path.join(__dirname, "../extensions")
+
+    await fs.writeFile(
+      path.join(dir, "languagepacks.json"),
+      `{"es":{"hash":"8d919a946475223861fa0c62665a4c50","extensions":[{"extensionIdentifier":{"id":"ms-ceintl.vscode-language-pack-es","uuid":"47e020a1-33db-4cc0-a1b4-42f97781749a"},"version":"1.70.0"}],"translations":{"vscode":"${extensionsDir}/ms-ceintl.vscode-language-pack-es-1.70.0/translations/main.i18n.json"},"label":"español"}}`,
+    )
     return dir
   }
 
