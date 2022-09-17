f36c1f5b919dcf8c8e8f3ed6b882af393dd72fda
Joe Previte <jjprevite@gmail.com>
2022-09-16 21:53:16 -0700

    refactor: move shellcheck to separate job

--- ci/dev/lint.sh
+++ ci/dev/lint.sh
@@ -6,7 +6,6 @@ main() {
 
   eslint --max-warnings=0 --fix $(git ls-files "*.ts" "*.tsx" "*.js" | grep -v "lib/vscode")
   tsc --noEmit --skipLibCheck
-  shellcheck -e SC2046,SC2164,SC2154,SC1091,SC1090,SC2002 $(git ls-files "*.sh" | grep -v "lib/vscode")
   if command -v helm && helm kubeval --help > /dev/null; then
     helm kubeval ci/helm-chart
   fi
