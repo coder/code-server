93f6fd24824a43d61bd3538eaa79fe842890990f
Joe Previte <jjprevite@gmail.com>
2022-09-16 21:27:46 -0700

    refactor: remove stylelint

--- ci/dev/lint.sh
+++ ci/dev/lint.sh
@@ -5,7 +5,6 @@ main() {
   cd "$(dirname "$0")/../.."
 
   eslint --max-warnings=0 --fix $(git ls-files "*.ts" "*.tsx" "*.js" | grep -v "lib/vscode")
-  stylelint $(git ls-files "*.css" | grep -v "lib/vscode")
   tsc --noEmit --skipLibCheck
   shellcheck -e SC2046,SC2164,SC2154,SC1091,SC1090,SC2002 $(git ls-files "*.sh" | grep -v "lib/vscode")
   if command -v helm && helm kubeval --help > /dev/null; then
