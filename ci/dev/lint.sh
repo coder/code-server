3028e8ebf8622d6423cd56249d0d255802e9b1f5
Joe Previte <jjprevite@gmail.com>
2022-09-16 22:17:32 -0700

    refactor: add helm script and job

--- ci/dev/lint.sh
+++ ci/dev/lint.sh
@@ -6,9 +6,6 @@ main() {
 
   eslint --max-warnings=0 --fix $(git ls-files "*.ts" "*.tsx" "*.js" | grep -v "lib/vscode")
   tsc --noEmit --skipLibCheck
-  if command -v helm && helm kubeval --help > /dev/null; then
-    helm kubeval ci/helm-chart
-  fi
 
   cd "$OLDPWD"
 }
