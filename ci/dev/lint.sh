d8e0dc96ac7e8e69497447eea7e5d3543f5927d7
Joe Previte <jjprevite@gmail.com>
2022-09-16 22:25:45 -0700

    refactor: add eslint job and yarn script

--- ci/dev/lint.sh
+++ ci/dev/lint.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 main() {
   cd "$(dirname "$0")/../.."
 
-  eslint --max-warnings=0 --fix $(git ls-files "*.ts" "*.tsx" "*.js" | grep -v "lib/vscode")
   tsc --noEmit --skipLibCheck
 
   cd "$OLDPWD"
