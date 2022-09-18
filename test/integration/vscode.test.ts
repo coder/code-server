13286bf4c9762b7d6c6176464d94b1fc316fb568
Joe Previte <jjprevite@gmail.com>
2022-09-17 20:20:01 -0700

    wip: refactor vscode integration tests

--- test/integration/vscode.test.ts
+++ test/integration/vscode.test.ts
@@ -1,11 +1,10 @@
 import { promises as fs } from "fs"
 import * as path from "path"
-import { clean, tmpdir } from "../../../utils/helpers"
-import * as httpserver from "../../../utils/httpserver"
-import * as integration from "../../../utils/integration"
+import { clean, tmpdir } from "../utils/helpers"
+import * as httpserver from "../utils/httpserver"
+import * as integration from "../utils/integration"
 
-// TODO@jsjoeio - move these to integration tests since they rely on Code
-// to be built
+// TODO@jsjoeio - remove skip when script fixed
 describe.skip("vscode", () => {
   let codeServer: httpserver.HttpServer | undefined
 
