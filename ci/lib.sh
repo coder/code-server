f08350bb6121aa83f367ed12f9add0d54dcea28f
Asher <ash@coder.com>
2022-09-22 13:53:25 -0500

    hm

--- ci/lib.sh
+++ ci/lib.sh
@@ -56,5 +56,5 @@ OS=$(os)
 export OS
 
 # RELEASE_PATH is the destination directory for the release from the root.
-# Defaults to release
+# Defaults to release!!!!!!!!!!!!
 RELEASE_PATH="${RELEASE_PATH-release}"
