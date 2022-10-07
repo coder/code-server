7a75bd08007b226ee6d31d9a9ef0e23165deb938
Joe Previte <jjprevite@gmail.com>
2022-10-07 15:14:49 -0700

    feat: add test for VSCODE_PROXY_URI

--- test/e2e/asExternalUri.test.ts
+++ test/e2e/asExternalUri.test.ts
@@ -2,8 +2,6 @@ import * as path from "path"
 import { getMaybeProxiedCodeServer } from "../utils/helpers"
 import { describe, test, expect } from "./baseFixture"
 
-// TODO@jsjoeio - account for proxy
-
 const flags = ["--extensions-dir", path.join(__dirname, "./extensions")]
 describe("asExternalUri", flags, {}, () => {
   test("should use /proxy/port", async ({ codeServerPage }) => {
@@ -24,3 +22,27 @@ describe("asExternalUri", flags, {}, () => {
     expect(text).toBe(`Info: input: ${input} output: ${expected}`)
   })
 })
+
+describe(
+  "asExternalUri",
+  flags,
+  { VSCODE_PROXY_URI: "https://{{port}}-main-workspace-name-user-name.coder.com" },
+  () => {
+    test("should use VSCODE_PROXY_URI", async ({ codeServerPage }) => {
+      // Given
+      const port = "3000"
+      const input = `http://localhost:${port}`
+      const expected = `https://${port}-main-workspace-name-user-name.coder.com/`
+
+      // When
+      await codeServerPage.waitForTestExtensionLoaded()
+      await codeServerPage.executeCommandViaMenus("code-server: asExternalUri test")
+      await codeServerPage.page.type(".quick-input-widget", input)
+      await codeServerPage.page.keyboard.press("Enter")
+
+      // Then
+      const text = await codeServerPage.page.locator("text=output").first().textContent()
+      expect(text).toBe(`Info: input: ${input} output: ${expected}`)
+    })
+  },
+)
