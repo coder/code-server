21cb2de18577d426c01547af5b7bb97e057fed65
Joe Previte <jjprevite@gmail.com>
2022-10-07 11:12:05 -0700

    feat(testing): add asExternalUri
    This modifies the test extension used in e2e test by registering a new
    command for testing `asExternalUri`.

--- test/e2e/extensions/test-extension/extension.ts
+++ test/e2e/extensions/test-extension/extension.ts
@@ -2,12 +2,29 @@ import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.window.showInformationMessage("test extension loaded")
+  // Test extension
   context.subscriptions.push(
     vscode.commands.registerCommand("codeServerTest.proxyUri", () => {
       if (process.env.VSCODE_PROXY_URI) {
         vscode.window.showInformationMessage(`proxyUri: ${process.env.VSCODE_PROXY_URI}`)
       } else {
         vscode.window.showErrorMessage("No proxy URI was set")
+      }
+    }),
+  )
+
+  // asExternalUri extension
+  context.subscriptions.push(
+    vscode.commands.registerCommand("codeServerTest.asExternalUri", async () => {
+      const input = await vscode.window.showInputBox({
+        prompt: "URL to pass through to asExternalUri",
+      })
+
+      if (input) {
+        const output = await vscode.env.asExternalUri(vscode.Uri.parse(input))
+        vscode.window.showInformationMessage(`input: ${input} output: ${output}`)
+      } else {
+        vscode.window.showErrorMessage(`Failed to run test case. No input provided.`)
       }
     }),
   )
