6c8db6c9957d23bacc901978d220f33a430fa8be
Joe Previte <jjprevite@gmail.com>
2022-12-20 15:59:33 -0700

    feat: add codeWorkspace to global setup

--- test/utils/globalE2eSetup.ts
+++ test/utils/globalE2eSetup.ts
@@ -1,6 +1,8 @@
 import { workspaceDir } from "./constants"
-import { clean } from "./helpers"
+import { clean, tmpdir } from "./helpers"
 import * as wtfnode from "./wtfnode"
+import * as path from "path"
+import { promises as fs } from "fs"
 
 /**
  * Perform workspace cleanup and authenticate. This should be ran before e2e
@@ -16,6 +18,11 @@ export default async function () {
   if (process.env.WTF_NODE) {
     wtfnode.setup()
   }
+
+  // Create dummy code-workspace
+  const codeWorkspace = path.join(await tmpdir(workspaceDir), "test.code-workspace")
+  await fs.writeFile(codeWorkspace, "")
+  process.env.CODE_WORKSPACE_DIR = codeWorkspace
 
   console.log("✅ Global Setup for Playwright End-to-End Tests is now complete.")
 }
