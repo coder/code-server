4f292140af74912830ae0ee59fb495734baf4b57
Chip Lee <chiplee@gmail.com>
2024-01-04 11:54:46 +0400

    conda and docker

---
+++ installs/r.sh
@@ -0,0 +1,6 @@
+export NS=cs-chip-gpu1
+helm uninstall $NS -n $NS
+sleep 10
+helm upgrade --install $NS ../ci/helm-chart -f values-base.yaml -f values-1gpux.yaml -n $NS --create-namespace
+echo $(kubectl get secret --namespace $NS "$NS-code-server" -o jsonpath="{.data.password}" | base64 --decode)
+kubectl get svc -n $NS
