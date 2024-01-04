3d136da7d83f22fb70c40c7e69e9f3edc0056fe7
Chip Lee <chiplee@gmail.com>
2024-01-04 12:06:44 +0400

    fix reload

--- installs/r.sh
+++ installs/r.sh
@@ -1,6 +1,7 @@
 export NS=cs-chip-gpu1
 helm uninstall $NS -n $NS
 sleep 10
-helm upgrade --install $NS ../ci/helm-chart -f values-base.yaml -f values-1gpux.yaml -n $NS --create-namespace
+helm upgrade --install $NS ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n $NS --create-namespace
 echo $(kubectl get secret --namespace $NS "$NS-code-server" -o jsonpath="{.data.password}" | base64 --decode)
 kubectl get svc -n $NS
+watch -n 1 kubectl get po -n $NS
