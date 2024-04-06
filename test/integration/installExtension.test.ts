73e615da4e78fa28b71331e4c31913099aa887b4
Asher <ash@coder.com>
2024-04-05 17:22:51 -0800

    Fix extension installation test
    This throws now that the exit is non-zero.

--- test/integration/installExtension.test.ts
+++ test/integration/installExtension.test.ts
@@ -13,9 +13,10 @@ describe("--install-extension", () => {
   })
   it("should use EXTENSIONS_GALLERY when set", async () => {
     const extName = "author.extension"
-    const { stderr } = await runCodeServerCommand([...setupFlags, "--install-extension", extName], {
-      EXTENSIONS_GALLERY: "{}",
-    })
-    expect(stderr).toMatch("No extension gallery service configured")
+    await expect(
+      runCodeServerCommand([...setupFlags, "--install-extension", extName], {
+        EXTENSIONS_GALLERY: "{}",
+      }),
+    ).rejects.toThrow("No extension gallery service configured")
   })
 })
