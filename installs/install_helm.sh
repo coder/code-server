e40bd85167f038db5f7d6683e7dc1b4ddd3a0b71
Chip Lee <chiplee@gmail.com>
2024-05-21 10:33:31 +0400

    new users

--- installs/install_helm.sh
+++ installs/install_helm.sh
@@ -17,13 +17,16 @@ helm upgrade --install cs-ws-gpu3 ../ci/helm-chart -f values-base-docker.yaml -f
 
 ## Bright Cluster
 helm upgrade --install cs-ridwan2 ../ci/helm-chart -f values-base-docker.yaml -f values-4gpu.yaml -f values-bright-mounts.yaml -n cs-ridwan2 --create-namespace
-helm upgrade --install cs-akhmed ../ci/helm-chart -f values-base-docker.yaml -f values-4gpu.yaml -f values-bright-mounts.yaml -n cs-akhmed --create-namespace
-
-helm upgrade --install cs-ridwan ../ci/helm-chart -f values-base-docker.yaml -f values-3gpu.yaml -f values-bright-mounts.yaml -n cs-ridwan --create-namespace
 helm upgrade --install cs-chong ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -f values-bright-mounts.yaml -n cs-chong --create-namespace
 
 ## K3S
 helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-gpu-chip --create-namespace
 
 ## AIOS
-helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-nodocker.yaml -f values-1gpu.yaml -f values-bright-mounts.yaml -n cs-gpu-chip --create-namespace
+helm upgrade --install cs-akhmed ../ci/helm-chart -f values-base-docker.yaml -f values-4gpu.yaml -f values-bright-mounts.yaml -n cs-akhmed --create-namespace
+helm upgrade --install cs-ridwan ../ci/helm-chart -f values-base-docker.yaml -f values-3gpu.yaml  -f values-bright-mounts.yaml -n cs-ridwan --create-namespace
+helm upgrade --install cs-hanshuo ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml  -f values-bright-mounts.yaml -n cs-hanshuo --create-namespace
+
+
+## AIOS-ws
+helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu-shared.yaml -n cs-gpu-chip --create-namespace
