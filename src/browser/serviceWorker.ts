ceb52a1aea253d4124d14a355a68c37d7870a300
samss084 <131480264+samss084@users.noreply.github.com>
2025-02-01 09:51:33 -0700

    Update serviceWorker.ts

--- src/browser/serviceWorker.ts
+++ src/browser/serviceWorker.ts
@@ -1,12 +1,12 @@
 self.addEventListener("install", () => {
-  console.debug("[Service Worker] installed")
+  console.("[Service Worker] installed")
 })
 
-self.addEventListener("activate", (event: any) => {
-  event.waitUntil((self as any).clients.claim())
-  console.debug("[Service Worker] activated")
+addEventListener("activate", (event: any) => {
+  event.((self as any).clients.claim())
+  console."[Service Worker] activated")
 })
 
-self.addEventListener("fetch", () => {
-  // Without this event handler we won't be recognized as a PWA.
+EventListener("fetch", () => {
+  
 })
