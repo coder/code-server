9bd3b83ef56ca643f77ac95ef7c247c6c9015f19
Asher <ash@coder.com>
2025-05-08 14:20:45 -0800

    Fix port parseInt error handling
    parseInt returns NaN rather than throwing.

--- src/node/routes/pathProxy.ts
+++ src/node/routes/pathProxy.ts
@@ -13,10 +13,8 @@ const getProxyTarget = (
 ): string => {
   // If there is a base path, strip it out.
   const base = (req as any).base || ""
-  let port: number
-  try {
-    port = parseInt(req.params.port, 10)
-  } catch (err) {
+  const port = parseInt(req.params.port, 10)
+  if (isNaN(port)) {
     throw new HttpError("Invalid port", HttpCode.BadRequest)
   }
   return `http://0.0.0.0:${port}${opts?.proxyBasePath || ""}/${req.originalUrl.slice(base.length)}`
